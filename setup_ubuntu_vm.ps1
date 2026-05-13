dc904d5de95bcdd65797cc02ebd3b7c8ca391a44
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-05-13 15:37:11 +0800

    add scripts for debian and ubuntu VM

---
+++ setup_ubuntu_vm.ps1
@@ -0,0 +1,31 @@
+New-VMSwitch -Name "SWITCH" -SwitchType Internal
+New-NetIPAddress `
+  -InterfaceAlias "vEthernet (SWITCH)" `
+  -IPAddress 192.168.100.1 `
+  -PrefixLength 24
+New-NetNat `
+  -Name "HyperVNAT" `
+  -InternalIPInterfaceAddressPrefix 192.168.100.0/24
+
+ssh-keygen.exe -t rsa
+$env:Path += ";C:\qemu"
+
+cd C:\Users\Administrator\Hyper-V-Automation-master
+
+$imgFile = .\Get-UbuntuImage.ps1 -Verbose
+$vmName = 'TstUbuntu'
+$fqdn = 'test.example.com'
+$rootPublicKey = Get-Content "$env:USERPROFILE\.ssh\id_rsa.pub"
+
+.\New-VMFromUbuntuImage.ps1 `
+    -SourcePath $imgFile `
+    -VMName $vmName `
+    -FQDN $fqdn `
+    -RootPublicKey $rootPublicKey `
+    -VHDXSizeBytes 20GB `
+    -MemoryStartupBytes 2GB `
+    -ProcessorCount 2 `
+    -IPAddress 192.168.100.10/24 `
+    -Gateway 192.168.100.1 `
+    -DnsAddresses '8.8.8.8','8.8.4.4' `
+    -Verbose
