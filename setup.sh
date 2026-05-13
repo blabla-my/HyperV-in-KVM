0c169cecaaf29b83c9a161fe87da2be0abdb90d6
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-05-13 11:11:03 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-qemu-img create -f qcow2 win_hdd.qcow2 40G
+wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.285-1/virtio-win-0.1.285.iso -O virtio-win.iso
+wget https://go.microsoft.com/fwlink/\?linkid\=2345730\&clcid\=0x409\&culture\=en-us\&country\=us -O winserver2025.iso
+qemu-img create -f qcow2 win_hdd.qcow2 128G
 
